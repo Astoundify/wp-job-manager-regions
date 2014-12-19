0cc3e099bdede51643252faec2683f9ce86aa4da
Spencer Finnell <spencerfinnell@gmail.com>
2014-12-19 12:11:00 -0500

    Only output a linked list on singular pages. Closes #26

--- includes/class-template.php
+++ includes/class-template.php
@@ -88,6 +88,17 @@ class Astoundify_Job_Manager_Regions_Template extends Astoundify_Job_Manager_Reg
 	 * @since 1.0.0
 	 */
 	public function the_job_location( $job_location, $post ) {
-		return get_the_term_list( $post->ID, 'job_listing_region', '', ', ', '' );
+		if ( is_singular( 'job_listing' ) ) {
+			return get_the_term_list( $post->ID, 'job_listing_region', '', ', ', '' );
+		} else {
+			$terms = wp_get_object_terms( $post->ID, 'job_listing_region' );
+			$names = array();
+
+			foreach ( $terms as $term ) {
+				$names[] = $term->name;
+			}
+
+			return implode( ', ', $names );
+		}
 	}
 }
