40eebe3d9db70b7bf8dbde9d673f753385620a1e
Spencer Finnell <spencerfinnell@gmail.com>
2019-01-14 11:43:21 -0500

    Ensure select2 is enqueued.

--- includes/class-template.php
+++ includes/class-template.php
@@ -32,13 +32,19 @@ class Astoundify_Job_Manager_Regions_Template extends Astoundify_Job_Manager_Reg
 	public function wp_enqueue_scripts() {
 		$deps = array( 'jquery' );
 
-		if ( wp_script_is( 'chosen' ) ) {
-			$deps[] = 'chosen';
+		$wpjm = WPJM();
+
+		if ( method_exists( $wpjm, 'register_select2_assets' ) ) {
+			$wpjm::register_select2_assets();
+
+			wp_enqueue_script( 'select2' );
+			wp_enqueue_style( 'select2' );
 		} else {
-			$deps[] = 'select2';
+			wp_enqueue_script( 'chosen' );
+			wp_enqueue_style( 'chosen' );
 		}
 
-		wp_enqueue_script( 'job-regions', wp_job_manager_regions()->plugin_url . 'assets/js/main.js', $deps, 20140525, true );
+		wp_enqueue_script( 'job-regions', wp_job_manager_regions()->plugin_url . 'assets/js/main.js', array( 'jquery' ), 20140525, true );
 	}
 
 	public function submit_resume_form_fields_get_resume_data( $fields, $job ) {
